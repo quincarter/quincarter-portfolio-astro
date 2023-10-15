d955c13357cf34d5e88f5c5d91406c594c02208c
Quin Carter <quin.carter@gmail.com>
2023-10-15 16:57:52 -0500

    updated metadata - description and names

--- src/utils/AppConfig.ts
+++ src/utils/AppConfig.ts
@@ -1,8 +1,8 @@
 export const AppConfig = {
-  site_name: 'Astro boilerplate',
-  title: 'Astro boilerplate',
-  description: 'Boilerplate built with Astro using React and Tailwind CSS',
-  author: 'Emma',
+  site_name: 'Quin Carter - Portfolio',
+  title: 'Quin Carter - Portfolio',
+  description: "This is Quin Carter's portfolio!",
+  author: 'Quin Carter',
   locale_region: 'en-us',
   locale: 'en',
 };
