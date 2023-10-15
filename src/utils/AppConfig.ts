fa4295122fb065283ce76b5a940e1a2609a66d2d
Quin Carter <quin.carter@gmail.com>
2023-10-15 18:38:37 -0500

    Updated app config description

--- src/utils/AppConfig.ts
+++ src/utils/AppConfig.ts
@@ -1,9 +1,8 @@
-import { MAIN_HERO_DATA } from '@/pages/page-data/main-hero';
-
 export const AppConfig = {
   site_name: 'Quin Carter - Portfolio',
   title: 'Quin Carter - Portfolio',
-  description: MAIN_HERO_DATA.description,
+  description:
+    'I am currently a Lead Software Engineer at Capital One. I have a huge focus on Frontend Architecture and Design',
   author: 'Quin Carter',
   locale_region: 'en-us',
   locale: 'en',
