185d61c7ec9e054c16032647bfbe249b09ccf8ad
Quin Carter <quin.carter@gmail.com>
2023-10-15 18:29:19 -0500

    added app config

--- src/utils/AppConfig.ts
+++ src/utils/AppConfig.ts
@@ -1,7 +1,9 @@
+import { MAIN_HERO_DATA } from '@/pages/page-data/main-hero';
+
 export const AppConfig = {
   site_name: 'Quin Carter - Portfolio',
   title: 'Quin Carter - Portfolio',
-  description: "This is Quin Carter's portfolio!",
+  description: MAIN_HERO_DATA.description,
   author: 'Quin Carter',
   locale_region: 'en-us',
   locale: 'en',
