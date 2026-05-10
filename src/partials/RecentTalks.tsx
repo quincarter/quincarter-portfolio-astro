2092042cc634b250d5d6332ff123bddb0d3c10ee
Quin Carter <quin.carter@gmail.com>
2026-05-09 23:09:46 -0400

    Added talks page and 2 talks from last year (#7)
    * Added talks page and 2 talks from last year
    
    * Updated index
    
    * Updated PR comments

---
+++ src/partials/RecentTalks.tsx
@@ -0,0 +1,35 @@
+import '../styles/blog-gallery.css';
+
+import type { MarkdownInstance } from 'astro';
+import type { IFrontmatter } from 'astro-boilerplate-components';
+import {
+	BlogGallery,
+	GradientText,
+	Section,
+} from 'astro-boilerplate-components';
+
+type IRecentTalksProps = {
+	talkList: MarkdownInstance<IFrontmatter>[];
+};
+
+const RecentTalks = (props: IRecentTalksProps) => (
+	<Section
+		title={
+			<div className="flex items-baseline justify-between">
+				<div>
+					Recent <GradientText>Talks</GradientText>
+				</div>
+
+				<div className="text-sm">
+					<a href="/talks/">View all Talks →</a>
+				</div>
+			</div>
+		}
+	>
+		<div className="blog-gallery">
+			<BlogGallery postList={props.talkList} />
+		</div>
+	</Section>
+);
+
+export { RecentTalks };
