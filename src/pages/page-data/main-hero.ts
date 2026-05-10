08b8a6c3c3b1089c67366b011752161e96112af2
Quin Carter <quin.carter@gmail.com>
2026-05-09 22:10:18 -0400

    Added to main hero

--- src/pages/page-data/main-hero.ts
+++ src/pages/page-data/main-hero.ts
@@ -1,5 +1,7 @@
 export const MAIN_HERO_DATA = {
 	description: `I am currently a Lead Software Engineer at Capital One. I have a huge focus on Frontend Architecture and Design, and have led multiple projects across Capital One over the last ${
 		new Date().getFullYear() - 2019
-	} years. Please peruse my portfolio of projects I can share, articles I have written, and patents & awards I have won.`,
+	} years. But I have been a Software Engineer for the last ${
+		new Date().getFullYear() - 2011
+	} years (since about 2011ish). I started doing freelance work and gradually worked my way through different web projects until I eventually got into my first dev role at a school district in Texas. After that, I bounced around quite a bit until I landed at Capital One! The rest is history. Please peruse my portfolio of projects I can share, articles I have written, and patents & awards I have won.`,
 };
